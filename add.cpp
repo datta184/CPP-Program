def15e2ea3d101bc75d770371b658475b5d6e168
datta kakde <85458867+datta184@users.noreply.github.com>
2021-11-10 21:37:02 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter the frist number:";
+cin>>a;
+cout<<"enter the second number:";
+cin>>b;
+c=a+b;
+cout<<"the addition is:"<<c<<endl;
+}
