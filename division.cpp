583bfcfacd6eee38af7f9bdea5a082e333438697
datta kakde <85458867+datta184@users.noreply.github.com>
2021-11-16 22:14:00 +0530

    division of two number
    Enter the frist number:10
    Enter the second number:2
    the division is:5

---
+++ division.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m,s;
+    cout<<"Enter the frist number:";
+    cin>>n;
+    cout<<"Enter the second number:";
+    cin>>m;
+    cout<<"the division is:"<<n/m;
+}
+
+
