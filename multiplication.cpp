d592ce9917d3283ecea8544d6d08bef47fae0f0c
datta kakde <85458867+datta184@users.noreply.github.com>
2021-11-16 22:11:16 +0530

    multiplication of two number
    Enter the first number:5
    
    Enter the second number:5
    
    the multiplication is:25

---
+++ multiplication.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m,s;
+    cout<<"Enter the frist number:";
+    cin>>n;
+    cout<<"Enter the second number:";
+    cin>>m;
+    cout<<"the multiplication is:"<<n*m;
+}
