1117b9caabea503b0b313745013e3be04feebc2c
datta kakde <85458867+datta184@users.noreply.github.com>
2021-11-24 23:22:40 +0530

    Area of circle

---
+++ area_of_circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float r,area;
+	std::cout<<"enter the radius of circle:";
+	std::cin>>r;
+	area=3.14*r*r;
+	std::cout<<"the area of circle is:"<<area<<endl;
+}
